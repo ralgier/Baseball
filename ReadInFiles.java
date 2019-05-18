aebfa2e24f293fa98e651230666d3e4a3aded39a
ralgier <36775917+ralgier@users.noreply.github.com>
2019-05-18 16:36:32 -0400

    Add files via upload

--- ReadInFiles.java
+++ ReadInFiles.java
@@ -5,45 +5,35 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class ReadInFiles {
 
-	// read in the files?
 	public static void main(String args[]) {
 
 		// create an array list to add the names to
 		List<String> namesArray = new ArrayList<>();
 
-		// filename to read
-		String marioFile = "MarioBatters.csv";
-		marioFile = args[0];
-		String peachFile = "PeachBatters.csv";
-		peachFile = args[1];
-		String yoshiFile = "YoshiBatters.csv";
-		yoshiFile = args[2];
-		String dkFile = "DKBatters.csv";
-		dkFile = args[3];
-		String warioFile = "WarioBatters.csv";
-		warioFile = args[4];
-		String bowserFile = "BowserBatters.csv";
-		bowserFile = args[5];
-		int x = 0;
+		String team1 = args[0];
+//		String team2 = args[1];
 
 		try {
-			Scanner scanner = new Scanner(new File(args[x]));
+//			Scanner scanner = new Scanner(new File(args[x]));
+			Scanner scanner = new Scanner(new File(team1));
+			//skip the first line
+			scanner.next();
 			while (scanner.hasNextLine()) {
-				
-//				String[] arrayToSeparate = new String[1];
-//				List<String> list = new ArrayList<String>(Arrays.asList(arrayToSeparate.split(","))); 
-				System.out.println(scanner.nextLine());
-				
+				//set the String to 
+				String toSeparate = scanner.next();
+				String[] values = toSeparate.split(",");
+			    System.out.println(Arrays.toString(values));
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
 		}
-	}
-
-}
+		
+	}//end of main
+}//end of class
