743fc5b65ca9e342f8eff66d69172fa3d7845a7e
ralgier <36775917+ralgier@users.noreply.github.com>
2019-05-25 21:10:05 -0400

    Add files via upload

---
+++ Batters.java
@@ -0,0 +1,71 @@
+
+public class Batters {
+
+	public static void main(String[] args) {
+		
+		//name, single, double, triple, homer, walk
+		//Mario
+		Player GreenNoki = new Player("GreenNoki", 200, 60, 5, 10, 70);
+		Player Luigi = new Player("Luigi", 190, 55, 3, 40, 60);
+		Player Mario = new Player("Mario", 200, 75, 4, 50, 80);
+		Player Pianta = new Player("Pianta", 140, 25, 1, 75, 75);
+		Player Blooper = new Player("Blooper", 200, 45, 2, 20, 65);
+		Player BabyMario = new Player("BabyMario",180,55,4,5,50);
+		Player BabyLuigi = new Player("BabyLuigi",190,60,4,5,55);
+		Player BabyNoki = new Player("BlueNoki",205,50,3,12,70);
+		Player MontyMole = new Player("MontyMole",180,45,7,10,40);
+		//Bowser
+		Player DarkDryBones =new Player("DarkDryBones",200,52,5,60,70);
+		Player FireBro =new Player("FireBro",185,60,2,60,60);
+		Player BowserJr =new Player("BowserJr",190,75,3,65,60);
+		Player Bowser =new Player("Bowser",155,30,1,85,70);
+		Player HammerBro =new Player("HammerBro",175,45,2,65,50);
+		Player BoomerangBro =new Player("BoomerangBro",180,55,2,50,45);
+		Player BlueDryBones =new Player("BlueDryBones",175,50,4,20,55);
+		Player DryBones =new Player("DryBones",170,55,4,30,60);
+		Player GreenDryBones =new Player("GreenDryBones",155,60,5,20,66);
+		//DK
+		Player DiddyKong =new Player("DiddyKong",210,60,6,10,90);
+		Player DixieKong =new Player("DixieKong",195,50,5,5,70);
+		Player DK =new Player("DK",190,40,2,50,40);
+		Player KingKRool =new Player("KingKRool",175,35,1,75,40);
+		Player FunkyKong =new Player("FunkyKong",180,40,2,45,50);
+		Player BabyDK =new Player("BabyDK",190,45,4,30,50);
+		Player TinyKong =new Player("TinyKong",185,50,4,15,75);
+		Player GreenKritter =new Player("GreenKritter",160,35,2,65,20);
+		Player RedKritter =new Player("RedKritter",150,35,1,60,30);
+		//Peach
+		Player Toadette =new Player("Toadette",225,80,8,8,75);
+		Player Peach =new Player("Peach",210,60,3,30,60);
+		Player Daisy =new Player("Daisy",190,50,3,65,50);
+		Player Petey =new Player("Petey",160,13,1,100,80);
+		Player Toad =new Player("Toad",200,55,4,60,65);
+		Player YellowToad =new Player("YellowToad",190,60,5,55,70);
+		Player BabyPeach =new Player("BabyPeach",160,40,4,5,50);
+		Player BabyDaisy =new Player("BabyDaisy",170,35,4,5,55);
+		Player Toadsworth =new Player("Toadsworth",185,35,2,6,40);
+		//Wario
+		Player Koopa =new Player("Koopa",220,50,5,55,70);
+		Player Boo =new Player("Boo",190,40,3,40,55);
+		Player Waluigi =new Player("Waluigi",200,40,2,35,80);
+		Player Wario =new Player("Wario",140,25,1,70,45);
+		Player KingBoo =new Player("KingBoo",185,35,2,50,45);
+		Player Paratroopa =new Player("Paratroopa",185,45,4,20,62);
+		Player Goomba =new Player("Goomba",165,45,2,10,50);
+		Player Magickoopa =new Player("Magickoopa",170,50,2,13,70);
+		Player Paragoomba =new Player("Paragoomba",155,60,6,11,50);
+		//Yoshi
+		Player Yoshi =new Player("Yoshi",230,85,10,20,80);
+		Player YellowShyGuy =new Player("YellowShyGuy",195,50,3,50,50);
+		Player Birdo =new Player("Birdo",160,40,2,60,80);
+		Player Wiggler =new Player("Wiggler",170,60,3,50,75);
+		Player RedShyGuy =new Player("RedShyGuy",180,50,2,35,60);
+		Player BlueYoshi =new Player("BlueYoshi",200,70,6,10,55);
+		Player BlackShyGuy =new Player("BlackShyGuy",190,40,2,45,50);
+		Player YellowYoshi =new Player("YellowYoshi",205,55,4,12,35);
+		Player BlueShyGuy =new Player("BlueShyGuy",140,40,2,25,40);
+		
+		
+	}
+
+}
