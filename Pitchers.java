2bace4664ba760bb91d1f5cf6fb9688c9abe9339
ralgier <36775917+ralgier@users.noreply.github.com>
2019-05-25 22:31:21 -0400

    Each Team's Pitcher

---
+++ Pitchers.java
@@ -0,0 +1,52 @@
+
+public class Pitchers {
+	
+	//Mario
+	static Pitcher MarioStarter = new Pitcher("MarioStarter", 100, 100);
+	static Pitcher MarioReliever1 = new Pitcher("MarioReliever1", 100, 100);
+	static Pitcher MarioReliever2 = new Pitcher("MarioReliever2", 100, 100);
+	static Pitcher MarioReliever3 = new Pitcher("MarioReliever3", 100, 100);
+	static Pitcher MarioCloser = new Pitcher("MarioCloser", 100, 100);
+	static Pitcher MarioBasePitcher = new Pitcher("MarioBasePitcher", -200, 9999999);
+	
+	//Bowser
+	static Pitcher BowserStarter = new Pitcher("BowserStarter", 100, 100);
+	static Pitcher BowserReliever1 = new Pitcher("BowserReliever1", 100, 100);
+	static Pitcher BowserReliever2 = new Pitcher("BowserReliever2", 100, 100);
+	static Pitcher BowserReliever3 = new Pitcher("BowserReliever3", 100, 100);
+	static Pitcher BowserCloser = new Pitcher("BowserMarioCloser", 100, 100);
+	static Pitcher BowserBasePitcher = new Pitcher("BowserBasePitcher", -200, 9999999);
+	
+	//DK
+	static Pitcher DKStarter = new Pitcher("DKStarter", 100, 100);
+	static Pitcher DKReliever1 = new Pitcher("DKReliever1", 100, 100);
+	static Pitcher DKReliever2 = new Pitcher("DKReliever2", 100, 100);
+	static Pitcher DKReliever3 = new Pitcher("DKReliever3", 100, 100);
+	static Pitcher DKCloser = new Pitcher("DKCloser", 100, 100);
+	static Pitcher DKBasePitcher = new Pitcher("DKBasePitcher", -200, 9999999);
+	
+	//Peach
+	static Pitcher PeachStarter = new Pitcher("PeachStarter", 100, 100);
+	static Pitcher PeachReliever1 = new Pitcher("PeachReliever1", 100, 100);
+	static Pitcher PeachReliever2 = new Pitcher("PeachReliever2", 100, 100);
+	static Pitcher PeachReliever3 = new Pitcher("PeachReliever3", 100, 100);
+	static Pitcher PeachCloser = new Pitcher("PeachCloser", 100, 100);
+	static Pitcher PeachBasePitcher = new Pitcher("PeachBasePitcher", -200, 9999999);
+	
+	//Wario
+	static Pitcher WarioStarter = new Pitcher("WarioStarter", 100, 100);
+	static Pitcher WarioReliever1 = new Pitcher("WarioReliever1", 100, 100);
+	static Pitcher WarioReliever2 = new Pitcher("WarioReliever2", 100, 100);
+	static Pitcher WarioReliever3 = new Pitcher("WarioReliever3", 100, 100);
+	static Pitcher WarioCloser = new Pitcher("WarioCloser", 100, 100);
+	static Pitcher WarioBasePitcher = new Pitcher("WarioBasePitcher", -200, 9999999);
+	
+	//Yoshi
+	static Pitcher YoshiStarter = new Pitcher("YoshiStarter", 100, 100);
+	static Pitcher YoshiReliever1 = new Pitcher("YoshiReliever1", 100, 100);
+	static Pitcher YoshiReliever2 = new Pitcher("YoshiReliever2", 100, 100);
+	static Pitcher YoshiReliever3 = new Pitcher("YoshiReliever3", 100, 100);
+	static Pitcher YoshiCloser = new Pitcher("YoshiCloser", 100, 100);
+	static Pitcher YoshiBasePitcher = new Pitcher("YoshiBasePitcher", -200, 9999999);
+
+}
