ade5f322439ba8a93d9fb19fab36045fd7310e11
Sflynn5 <38513810+Sflynn5@users.noreply.github.com>
2019-08-07 20:48:50 -0400

    Update Team.java

--- Team.java
+++ Team.java
@@ -5,11 +5,72 @@ public class Team {
 	ArrayDeque<Pitcher> pitchers = new ArrayDeque<Pitcher>();
 	ArrayDeque<Player> batters = new ArrayDeque<Player>();
 	String teamName;
+	int numWins;
+	int numLoss;
+	int RS;
+	int RA;
+	int RD;
+	int gamesPlayed;
 	
-	public Team(ArrayDeque<Pitcher> arms, ArrayDeque<Player> bats, String name) {
+	public Team(ArrayDeque<Pitcher> arms, ArrayDeque<Player> bats, String name, int numWins, int numLoss,
+			int RS, int RA, int RD, int gamesPlayed) {
 		this.pitchers = arms;
 		this.batters = bats;
 		this.teamName = name;
+		this.numWins = numWins;
+		this.numLoss = numLoss;
+		this.RS = RS;
+		this.RA = RA;
+		this.RD = RD;
+		this.gamesPlayed = gamesPlayed;
+	}
+
+	public int getNumWins() {
+		return numWins;
+	}
+
+	public void setNumWins(int numWins) {
+		this.numWins = numWins;
+	}
+
+	public int getNumLoss() {
+		return numLoss;
+	}
+
+	public void setNumLoss(int numLoss) {
+		this.numLoss = numLoss;
+	}
+
+	public int getRS() {
+		return RS;
+	}
+
+	public void setRS(int rS) {
+		RS = rS;
+	}
+
+	public int getRA() {
+		return RA;
+	}
+
+	public void setRA(int rA) {
+		RA = rA;
+	}
+
+	public int getRD() {
+		return RD;
+	}
+
+	public void setRD(int rD) {
+		RD = rD;
+	}
+
+	public int getGamesPlayed() {
+		return gamesPlayed;
+	}
+
+	public void setGamesPlayed(int gamesPlayed) {
+		this.gamesPlayed = gamesPlayed;
 	}
 
 	public String ToString() {
