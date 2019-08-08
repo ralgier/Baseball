74ba180d2961d519515e30d2676390d56e0dc033
Sflynn5 <38513810+Sflynn5@users.noreply.github.com>
2019-08-07 20:50:38 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,17 +1,18 @@
-
 public class main{
-
+	
+	static Team Fireballs = new Team(Pitchers.marioPitchers, Batters.mario, "Fireballs",0,0,0,0,0,0);
+	static Team Monsters = new Team(Pitchers.bowserPitchers, Batters.bowser, "Monsters",0,0,0,0,0,0);
+	static Team Roses = new Team(Pitchers.peachPitchers, Batters.peach, "Roses",0,0,0,0,0,0);
+	static Team Garlics = new Team(Pitchers.warioPitchers, Batters.wario, "Garlics",0,0,0,0,0,0);
+	static Team Explorers = new Team(Pitchers.dkPitchers, Batters.dk, "Explorers",0,0,0,0,0,0);
+	static Team Eggs = new Team(Pitchers.yoshiPitchers, Batters.yoshi, "Eggs",0,0,0,0,0,0);
+	
 	public static void main(String[] args) {
-		
-		Team Fireballs = new Team(Pitchers.marioPitchers, Batters.mario, "Fireballs");
-		Team Monsters = new Team(Pitchers.bowserPitchers, Batters.bowser, "Monsters");
-		Team Roses = new Team(Pitchers.peachPitchers, Batters.peach, "Roses");
-		Team Garlics = new Team(Pitchers.warioPitchers, Batters.wario, "Garlics");
-		Team Explorers = new Team(Pitchers.dkPitchers, Batters.dk, "Explorers");
-		Team Eggs = new Team(Pitchers.yoshiPitchers, Batters.yoshi, "Eggs");
-		
-		runGame test = new runGame (Fireballs, Roses);
-		System.out.println("Main class");
+			Batters.addBatters();
+			Pitchers.addPitcher();
+			for(int i = 0; i < 3; i++) {
+				runGame test = new runGame (Fireballs, Roses);
+			}
 	}
 
 }
