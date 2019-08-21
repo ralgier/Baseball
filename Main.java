36357abffecd1ef6dbab0a058280ed602aa03a87
ralgier <36775917+ralgier@users.noreply.github.com>
2019-08-21 19:05:15 -0400

    Add files via upload
    I am James Gosling

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.text.DecimalFormat;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -6,12 +7,11 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-
 //schedule, playoffs, check ind player stats at end of season
 
 public class Main {
 
-	//create all the teams
+	// create all the teams
 	static Team Fireballs = new Team(Pitchers.marioPitchers, Batters.mario, "Fireballs", 0, 0, 0, 0, 0, 0, 0);
 	static Team Monsters = new Team(Pitchers.bowserPitchers, Batters.bowser, "Monsters", 0, 0, 0, 0, 0, 0, 0);
 	static Team Roses = new Team(Pitchers.peachPitchers, Batters.peach, "Roses", 0, 0, 0, 0, 0, 0, 0);
@@ -19,30 +19,42 @@ public class Main {
 	static Team Explorers = new Team(Pitchers.dkPitchers, Batters.dk, "Explorers", 0, 0, 0, 0, 0, 0, 0);
 	static Team Eggs = new Team(Pitchers.yoshiPitchers, Batters.yoshi, "Eggs", 0, 0, 0, 0, 0, 0, 0);
 
+	ArrayDeque<Team> league1 = new ArrayDeque<Team>();
+
+	static int hrMax = 0;
+	static int rbiMax = 0;
+	static int hitMax = 0;
+	static double batAvgMax = 0;
+	static int singleMax = 0;
+	static int doubleMax = 0;
+	static int tripleMax = 0;
+	static int walkMax = 0;
+
 	public static void main(String[] args) {
-		//put all the teams in hashmap for season
+		// put all the teams in hashmap for season
 		HashMap<Integer, Team> teams = new HashMap<Integer, Team>();
+		ArrayDeque<HashMap<Integer, Player>> league1 = new ArrayDeque<HashMap<Integer, Player>>();
 		teams.put(1, Fireballs);
 		teams.put(2, Monsters);
 		teams.put(3, Roses);
 		teams.put(4, Garlics);
 		teams.put(5, Explorers);
 		teams.put(6, Eggs);
-
-		//add all the batters and pitchers (This is where we fix the stats issue -- DO NOT pput this in runGame)
+		// add all the batters and pitchers (This is where we fix the stats issue -- DO
+		// NOT pput this in runGame)
 		Batters.addBatters();
 		Pitchers.addPitcher();
 
-		//Order all the players to print stats
+		// Order all the players to print stats
 		HashMap<Integer, Player> FireballsMap = orderTeam(Fireballs);
 		HashMap<Integer, Player> RosesMap = orderTeam(Roses);
 		HashMap<Integer, Player> GarlicsMap = orderTeam(Garlics);
 		HashMap<Integer, Player> EggsMap = orderTeam(Eggs);
 		HashMap<Integer, Player> MonstersMap = orderTeam(Monsters);
 		HashMap<Integer, Player> ExplorersMap = orderTeam(Explorers);
-				
-		//run Games
-		for (int i = 1; i < 4; i++) {
+
+		// run Games
+		for (int i = 1; i < 32; i++) {
 			runSeasonGame.runGame(Fireballs, Roses);
 			runSeasonGame.runGame(Eggs, Explorers);
 			runSeasonGame.runGame(Monsters, Garlics);
@@ -60,7 +72,7 @@ public class Main {
 			runSeasonGame.runGame(Roses, Explorers);
 		}
 
-		//prints individual batters
+		// prints individual batters
 		printBattingStats(FireballsMap, Fireballs);
 		printBattingStats(RosesMap, Roses);
 		printBattingStats(GarlicsMap, Garlics);
@@ -68,61 +80,68 @@ public class Main {
 		printBattingStats(EggsMap, Eggs);
 		printBattingStats(ExplorersMap, Explorers);
 
-		//prints team standings
+		// prints team standings
 		printTeamSeasonStats(teams);
 
+		// Add ordered lineups to the league
+		league1.add(FireballsMap);
+		league1.add(RosesMap);
+		league1.add(EggsMap);
+		league1.add(MonstersMap);
+		league1.add(ExplorersMap);
+		league1.add(GarlicsMap);
+		// Find the leaders of categories
+		leaders(league1);
 	}
 
-	//method to print season stats
+	// method to print season stats
 	public static void printTeamSeasonStats(HashMap<Integer, Team> teams) {
 		System.out.println();
 		System.out.printf("%-10s %-6s %-6s %-7s %-5s %-5s %-5s", "Team Name", "Wins", "Losses", " PCT", "RS", "RA",
 				"RD");
 		HashMap<Team, Double> standings = new HashMap<Team, Double>();
 		for (Team x : teams.values()) {
-			standings.put(x, x.winPct); 
+			standings.put(x, x.winPct);
 		}
-		//calls sorting method
+		// calls sorting method
 		sortHashMapByValues(standings);
 	}
-	
-	//method to sort the teams by win pct
-	public static <T> LinkedHashMap<Team, Double> sortHashMapByValues(
-	        HashMap<Team, Double> passedMap) {
-	    List<Team> mapKeys = new ArrayList<>(passedMap.keySet());
-	    List<Double> mapValues = new ArrayList<>(passedMap.values());
-	    Collections.sort(mapValues, Collections.reverseOrder());
 
-	    LinkedHashMap<Team, Double> sortedMap =
-	        new LinkedHashMap<>();
+	// method to sort the teams by win pct
+	public static <T> LinkedHashMap<Team, Double> sortHashMapByValues(HashMap<Team, Double> passedMap) {
+		List<Team> mapKeys = new ArrayList<>(passedMap.keySet());
+		List<Double> mapValues = new ArrayList<>(passedMap.values());
+		Collections.sort(mapValues, Collections.reverseOrder());
 
-	    Iterator<Double> valueIt = mapValues.iterator();
-	    while (valueIt.hasNext()) {
-	        Double val = valueIt.next();
-	        Iterator<Team> keyIt = mapKeys.iterator();
+		LinkedHashMap<Team, Double> sortedMap = new LinkedHashMap<>();
 
-	        while (keyIt.hasNext()) {
-	            Team key = keyIt.next();
-	            Double comp1 = passedMap.get(key);
-	            Double comp2 = val;
+		Iterator<Double> valueIt = mapValues.iterator();
+		while (valueIt.hasNext()) {
+			Double val = valueIt.next();
+			Iterator<Team> keyIt = mapKeys.iterator();
 
-	            if (comp1.equals(comp2)) {
-	                keyIt.remove();
-	                sortedMap.put(key, val);
-	                break;
-	            }
-	        }
-	    }
-	    for (Team x : sortedMap.keySet()) {
+			while (keyIt.hasNext()) {
+				Team key = keyIt.next();
+				Double comp1 = passedMap.get(key);
+				Double comp2 = val;
+
+				if (comp1.equals(comp2)) {
+					keyIt.remove();
+					sortedMap.put(key, val);
+					break;
+				}
+			}
+		}
+		for (Team x : sortedMap.keySet()) {
 			System.out.println();
 			DecimalFormat df = new DecimalFormat("#.###");
 			System.out.printf("%-10s %-6s %-6s %-7s %-5s %-5s %-5s", x.teamName, "  " + x.numWins, "  " + x.numLoss,
 					df.format(x.winPct), x.RS, x.RA, x.RD);
-	    }
-	    return sortedMap;
+		}
+		return sortedMap;
 	}
-	
-	//prints individual stats method (one team at a time)
+
+	// prints individual stats method (one team at a time)
 	public static void printBattingStats(HashMap<Integer, Player> teamMap, Team team) {
 
 		int hits = 0;
@@ -157,8 +176,8 @@ public class Main {
 		System.out.println();
 		System.out.println(
 				"--------------------------------------------------------------------------------------------------------------");
-		System.out.printf("%15s %10s %10s %10s %10s %10s %10s %10s", "Total", hits + "-" + atBats, singles, doubles, triples,
-				homeruns, walks, rbi);
+		System.out.printf("%15s %10s %10s %10s %10s %10s %10s %10s", "Total", hits + "-" + atBats, singles, doubles,
+				triples, homeruns, walks, rbi);
 		battingAvg = (double) hits / (double) atBats;
 		System.out.println();
 		System.out.printf("Team Average: %.3f", battingAvg);
@@ -174,7 +193,7 @@ public class Main {
 		rbi = 0;
 	}
 
-	//method to order the teams by batting order
+	// method to order the teams by batting order
 	public static HashMap<Integer, Player> orderTeam(Team team) {
 		HashMap<Integer, Player> teamMap = new HashMap<Integer, Player>();
 		for (Player lead : team.batters) {
@@ -183,5 +202,132 @@ public class Main {
 		}
 		return teamMap;
 	}
+
+	// Method to get batting leaders in stats categories
+	public static void leaders(ArrayDeque<HashMap<Integer, Player>> league) {
+
+		// create lists to add the leaders in each category to
+		ArrayList<Player> HRLeaders = new ArrayList<Player>();
+		ArrayList<Player> RBILeaders = new ArrayList<Player>();
+		ArrayList<Player> HitLeaders = new ArrayList<Player>();
+		ArrayList<Player> BattingAvgLeaders = new ArrayList<Player>();
+		ArrayList<Player> SingleLeaders = new ArrayList<Player>();
+		ArrayList<Player> DoubleLeaders = new ArrayList<Player>();
+		ArrayList<Player> TripleLeaders = new ArrayList<Player>();
+		ArrayList<Player> WalkLeaders = new ArrayList<Player>();
+
+		int leagueSize = league.size();
+
+		DecimalFormat df = new DecimalFormat("#.###");
+		for (int i = 0; i < leagueSize; i++) {
+			for (int j = 1; j <= 9; j++) {
+				Player x = league.getFirst().get(j);
+				int hrTemp = x.numHomeruns;
+				int rbiTemp = x.rbi;
+				int hitTemp = x.numHits;
+				double batAvgTemp = ((double) x.numHits / (double) x.numAtBats);
+				int singleTemp = x.numSingles;
+				int doubleTemp = x.numDoubles;
+				int tripleTemp = x.numTriples;
+				int walkTemp = x.numWalks;
+
+				if (hrTemp > hrMax || hrTemp == hrMax) {
+					hrMax = hrTemp;
+					HRLeaders.add(x);
+				}
+				if (rbiTemp > rbiMax || rbiTemp == rbiMax) {
+					rbiMax = rbiTemp;
+					RBILeaders.add(x);
+				}
+				if (hitTemp > hitMax || hitTemp == hitMax) {
+					hitMax = hitTemp;
+					HitLeaders.add(x);
+				}
+				if (batAvgTemp > batAvgMax || batAvgTemp == batAvgMax) {
+					batAvgMax = batAvgTemp;
+					BattingAvgLeaders.add(x);
+				}
+				if (singleTemp > singleMax || singleTemp == singleMax) {
+					singleMax = singleTemp;
+					SingleLeaders.add(x);
+				}
+				if (doubleTemp > doubleMax || doubleTemp == doubleMax) {
+					doubleMax = doubleTemp;
+					DoubleLeaders.add(x);
+				}
+				if (tripleTemp > tripleMax || tripleTemp == tripleMax) {
+					tripleMax = tripleTemp;
+					TripleLeaders.add(x);
+				}
+				if (walkTemp > walkMax || walkTemp == walkMax) {
+					walkMax = walkTemp;
+					WalkLeaders.add(x);
+				}
+			}
+			league.remove();
+		}
+
+		System.out.println();
+		System.out.println("Season Leaders: ");
+
+		System.out.print("HR Leaders with " + hrMax + " homeruns:				");
+		for (Player test : HRLeaders) {
+			if (test.numHomeruns == hrMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("Single Leaders with " + singleMax + " singles:			");
+		for (Player test : SingleLeaders) {
+			if (test.numSingles == singleMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("Double Leaders with " + doubleMax + " doubles:				");
+		for (Player test : DoubleLeaders) {
+			if (test.numDoubles == doubleMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("Triple Leaders with " + tripleMax + " triples:				");
+		for (Player test : TripleLeaders) {
+			if (test.numTriples == tripleMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("Walk Leaders with " + walkMax + " walks:				");
+		for (Player test : WalkLeaders) {
+			if (test.numWalks == walkMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("RBI Leaders with " + rbiMax + " RBIs:				");
+		for (Player test : RBILeaders) {
+			if (test.rbi == rbiMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+		System.out.print("Hit Leaders with " + hitMax + " hits:				");
+		for (Player test : HitLeaders) {
+			if (test.numHits == hitMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+		System.out.printf("Batting Average Leaders with a " + df.format(batAvgMax) + " batting average:	");
+		for (Player test : BattingAvgLeaders) {
+			double batAvgTemp = ((double) test.numHits / (double) test.numAtBats);
+			if (batAvgTemp == batAvgMax) {
+				System.out.print(test.name + " ");
+			}
+		}
+		System.out.println();
+	}
+
 
 }
